c0805f3fe14bbfe9a23941a6c0b98b33c3750ab6
Isaias <99995191+isaias30silva@users.noreply.github.com>
2026-07-31 14:57:29 -0300

    feat: configuração de conexão com o banco de dados postgresql

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const sequelize = require("./src/config/database");
 
 const PORT = process.env.PORT || 3000;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("✅ Banco conectado com sucesso");
+  })
+  .catch((error) => {
+    console.error("❌ Erro ao conectar no banco:", error);
+  });
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
